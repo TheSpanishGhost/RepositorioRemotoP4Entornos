ead63839827da5d436f6a6bf8b90f14f678a26d1
Jorge <Jorge@DESKTOP-NHKARLC>
2025-03-30 18:19:04 +0200

    Versión 1.0

---
+++ Practica4Entornos/src/Practica4Entornos/Practica4__1_0.java
@@ -0,0 +1,15 @@
+package Practica4Entornos;
+
+public class Practica4__1_0 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Versión 1.0 pa la práctica de Entornos");
+		System.out.println("Blablablablabla *insertar mazo texto*");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("P.D: Caca culo pedo pis");
+	}
+}
