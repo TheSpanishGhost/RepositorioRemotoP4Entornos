fe52569cc33fcb7d8cbb6304cad5cf3ecae07a7a
Jorge <Jorge@DESKTOP-NHKARLC>
2025-03-30 23:44:24 +0200

    Versión 1.1 con cambios

--- Practica4Entornos/src/Practica4Entornos/Practica4__1_0.java
+++ Practica4Entornos/src/Practica4Entornos/Practica4__1_0.java
@@ -1,15 +1,29 @@
 package Practica4Entornos;
 
+import java.util.*;
+
 public class Practica4__1_0 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner (System.in);
 		
-		System.out.println("Versión 1.0 pa la práctica de Entornos");
+		System.out.println("Versión 1.1 con cambios");
 		System.out.println("Blablablablabla *insertar mazo texto*");
+		
+		int suma;
+		int n1;
+		int n2;
+		
+		System.out.println("Introduce dos números para sumarlos:");
+		n1 = sc.nextInt();
+		n2 = sc.nextInt();
+		suma = n1 + n2;
+		System.out.println("La suma de ambos es: " + suma);
 		System.out.println("\n");
 		System.out.println("\n");
 		System.out.println("\n");
 		System.out.println("P.D: Caca culo pedo pis");
+		
 	}
 }
